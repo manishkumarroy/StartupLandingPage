a4d11654b01898a71a23a3372f298759ff10f753
manishkumarroy <manishkumar222mani@gmail.com>
2021-03-18 01:53:51 +0530

    adding startup landing page

---
+++ script.js
@@ -0,0 +1,62 @@
+/*var dataBase = [ 
+	{
+	username:"sam",
+	password:"124",
+	},
+	{
+		username:"mansjh",
+		password:"777"
+	}
+	
+];
+
+
+var newsfeed = [
+	{
+		username:"mani",
+		timeline:"i am so tired"
+
+	},
+	{
+		username:"anu",
+		timeline:"you are too cool"
+	}
+
+
+];
+
+function isUservalid(username,password){
+	for(var i=0; i<dataBase.length; i++){
+		if(dataBase[i].username==username 
+			&& dataBase[i].password==password){
+			return true;
+		}
+		else{
+			return false; 
+		}
+	}
+}
+
+
+function signin(username,password){
+	if(isUservalid(username,password)){
+		console.log(newsfeed);
+	}
+	else{
+		alert("invalid username and password");
+	}
+}
+
+var usernameprompt= prompt("enter your user name");
+var userpasswordprompt= prompt("enter your password");
+
+signin(usernameprompt,userpasswordprompt);*/
+
+
+var x=132;
+var res=parseInt(('' + x))
+
+
+
+
+
